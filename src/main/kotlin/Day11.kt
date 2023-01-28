a380543d268525cbee4ab71331ed65ce3ea4fa72
Maciej Plis <maciej.plis.secure@gmail.com>
2023-01-28 22:01:30 +0100

    Day 11 Part 2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -2,54 +2,61 @@ import java.lang.IllegalArgumentException
 
 class Day11 {
 
-    fun solvePart1(input: String): Int {
+    fun solvePart1(input: String): Long {
         val monkeys = input.lines().chunked(7).map(Monkey::parse)
-
-        repeat(20) {
-            monkeys.forEach { it.inspectAllItems(monkeys) }
-        }
-
-        return monkeys.map(Monkey::inspections).sorted().takeLast(2).reduce { acc, i -> acc * i }
+        return solve(monkeys, 20) { it / 3 }
     }
 
-    fun solvePart2(input: String): Int {
-        return 0
+    fun solvePart2(input: String): Long {
+        val monkeys = input.lines().chunked(7).map(Monkey::parse)
+        val superMod = monkeys.fold(1L) { acc, monkey -> acc * monkey.divider }
+        return solve(monkeys, 10_000) { it % superMod }
+    }
+
+    private fun solve(monkeys: List<Monkey>, iterations: Int, reliefMethod: (Long) -> Long): Long {
+        repeat(iterations) {
+            monkeys.forEach { it.performInspections(monkeys, reliefMethod) }
+        }
+
+        return monkeys.topBy(2, Monkey::inspections).product()
     }
 
     private data class Monkey(
-        val items: ArrayDeque<Int>,
-        val inspectItem: (worry: Int) -> Int,
-        val whereToThrow: (worry: Int) -> Int,
-        var inspections: Int = 0
+        val items: ArrayDeque<Long>,
+        val inspection: (worry: Long) -> Long,
+        val whereToThrow: (worry: Long) -> Int,
+        val divider: Int,
+        var inspections: Long = 0
     ) {
 
-        fun inspectAllItems(monkeys: List<Monkey>) {
-            while(items.isNotEmpty()) {
+        fun performInspections(monkeys: List<Monkey>, relief: (Long) -> Long) {
+            while (items.isNotEmpty()) {
+                var itemWorry = items.removeFirst()
+                itemWorry = inspection(itemWorry)
+                itemWorry = relief(itemWorry)
+                monkeys[whereToThrow(itemWorry)].items.addLast(itemWorry)
                 inspections++
-                var item = items.removeFirst()
-                item = inspectItem(item)
-                item /= 3
-                monkeys[whereToThrow(item)].items.addLast(item)
             }
         }
 
         companion object {
             fun parse(lines: List<String>): Monkey {
-                val items = lines[1].drop(18).split(",").map { it.trim().toInt() }
-                val inspectItem = lines[2].removePrefix("  Operation: new = old ").let { getEquation(it[0], it.drop(2)) }
-                val whereToThrow = { worry: Int -> if(worry % lastInt(lines[3]) == 0) lastInt(lines[4]) else lastInt(lines[5])}
-                return Monkey(ArrayDeque(items), inspectItem, whereToThrow)
+                val items = lines[1].drop(18).split(",").map { it.trim().toLong() }
+                val inspectItem = lines[2].drop(23).let { createInspection(it[0], it.drop(2)) }
+                val divider = lines[3].lastInt()
+                val whereToThrow = { worry: Long -> if (worry % divider == 0L) lines[4].lastInt() else lines[5].lastInt() }
+                return Monkey(ArrayDeque(items), inspectItem, whereToThrow, divider)
             }
 
-            fun getEquation(operation: Char, value: String) = when (operation) {
-                '+' -> { worry: Int -> worry + parseValue(value, worry) }
-                '*' -> { worry: Int -> worry * parseValue(value, worry) }
+            private fun createInspection(operation: Char, value: String) = when (operation) {
+                '+' -> { worry: Long -> worry + (value.toLongOrNull() ?: worry) }
+                '*' -> { worry: Long -> worry * (value.toLongOrNull() ?: worry) }
                 else -> throw IllegalArgumentException("Unknown operation: $operation")
             }
 
-            fun parseValue(value: String, worry: Int): Int = if(value == "old") worry else value.toInt()
-
-            fun lastInt(line: String) = line.substringAfterLast(" ").toInt()
+            private fun String.lastInt() = substringAfterLast(' ').toInt()
         }
     }
+    private fun Iterable<Long>.product() = this.fold(1L) { acc, el -> acc * el }
+    private fun <T> Iterable<T>.topBy(n: Int, transform: (T) -> Long) = this.map(transform).sortedDescending().take(n)
 }
--- src/test/kotlin/Day11Test.kt
+++ src/test/kotlin/Day11Test.kt
@@ -10,35 +10,35 @@ internal class Day11Test {
     fun part1_testInput() {
         val input = readResourceFile("advent-of-code-2022/day-11-test").readText()
 
-        val output: Int = day11.solvePart1(input)
+        val output: Long = day11.solvePart1(input)
 
-        assertEquals(10605, output)
+        assertEquals(10_605, output)
     }
 
     @Test
     fun part1_fullInput() {
         val input = readResourceFile("advent-of-code-2022/day-11").readText()
 
-        val output: Int = day11.solvePart1(input)
+        val output: Long = day11.solvePart1(input)
 
-        assertEquals(121450, output)
+        assertEquals(121_450, output)
     }
 
     @Test
     fun part2_testInput() {
         val input = readResourceFile("advent-of-code-2022/day-11-test").readText()
 
-        val output: Int = day11.solvePart2(input)
+        val output: Long = day11.solvePart2(input)
 
-        assertEquals(-1, output)
+        assertEquals(2_713_310_158, output)
     }
 
     @Test
     fun part2_fullInput() {
         val input = readResourceFile("advent-of-code-2022/day-11").readText()
 
-        val output: Int = day11.solvePart2(input)
+        val output: Long = day11.solvePart2(input)
 
-        assertEquals(-1, output)
+        assertEquals(28_244_037_010, output)
     }
 }
